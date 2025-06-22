dc628cfd2949a1cf72a9b6da877ae807c0cd196e
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-06-22 13:10:58 +0300

    Initial commit

---
+++ src/test/java/com/ssva/mapserver/MapserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssva.mapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
