27f593b6ef2793b752aea7255756c9b4f5b515ea
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-06-22 14:04:06 +0300

    Since the MapServer Api returns a weird content type 'text/plain', WebClient by default does not support it, so to combat that the WebClient codec (encoder/decoder) was customized to firstly ignore the unknown fields that our model does not contain so random crashes won't happen. Then most importantly custom codec is being registered in addition to default ones which tells Spring to use this Jackson2JsonDecoder when it encounters a 'text/plain' type on the response and treat it like an actual JSON (which it already is). This fixes the problem, and now we are getting all the info required with an HTTP status of 200.

--- src/main/java/com/ssva/mapserver/configs/WebClientConfig.java
+++ src/main/java/com/ssva/mapserver/configs/WebClientConfig.java
@@ -1,16 +1,28 @@
 package com.ssva.mapserver.configs;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebClientConfig {
+    private final ObjectMapper objectMapper;
 
     @Bean
-    public WebClient webClient () {
+    public WebClient webClient() {
         return WebClient.builder()
+                .exchangeStrategies(ExchangeStrategies.builder().codecs(configurer -> {
+                    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                    configurer.customCodecs().register(new Jackson2JsonDecoder(objectMapper, MimeTypeUtils.parseMimeType(MediaType.TEXT_PLAIN_VALUE)));
+                }).build())
                 .build();
     }
-
 }
