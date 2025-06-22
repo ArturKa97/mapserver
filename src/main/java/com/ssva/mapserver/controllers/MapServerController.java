2b693ee4e288e282a4351a6e18104c33989cea01
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-06-22 13:46:08 +0300

    Currently, putting everything in a controller just to test the MapServer API call itself, however we are getting an UnsupportedMediaType exception that says  Content type 'text/plain;charset=utf-8' not supported, because although we are getting the response as a JSON, the Response Headers have a Content type of 'text/plain' and not JSON so the default WebClient does not support this content type.

--- src/main/java/com/ssva/mapserver/controllers/MapServerController.java
+++ src/main/java/com/ssva/mapserver/controllers/MapServerController.java
@@ -1,8 +1,30 @@
 package com.ssva.mapserver.controllers;
 
+import com.ssva.mapserver.models.MapServiceResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
+@RequiredArgsConstructor
 public class MapServerController {
+
+    private final WebClient webClient;
+
+    @GetMapping("/mapserver-info")
+    public Mono<ResponseEntity<MapServiceResponse>> getMapServerInfo (@RequestParam String url) {
+        //Checking if url already contains query params, if yes we add more to it, if not we add ?f=json to get the data as json from the API call
+        String jsonUrl = url.contains("?") ? url + "&f=json" : url + "?f=json";
+
+        return webClient.get()
+                .uri(jsonUrl)
+                .retrieve()
+                .bodyToMono(MapServiceResponse.class)
+                .map(ResponseEntity::ok);
+    }
 
 }
